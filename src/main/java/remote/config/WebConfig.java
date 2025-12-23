8d2699c4aa88e4229658a0bb417fc1c373b88c64
writecodes1 <rajsoni676254as@gmail.com>
2025-12-23 21:05:37 +0530

    added

--- src/main/java/remote/config/WebConfig.java
+++ src/main/java/remote/config/WebConfig.java
@@ -1,22 +1,18 @@
 package remote.config;
-import org.springframework.context.annotation.Bean;
+ 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig  implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://angular-spring-boot-project-git-main-writercodes1s-projects.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
